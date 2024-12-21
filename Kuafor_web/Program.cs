583f2cc93ec8ffca4208a39d6300556d802e926a
osman yıldız <yildiz03032004@hotmail.com>
2024-12-21 23:53:19 +0300

    view ve fonksiyon düzenlemeleri

--- Kuafor_web/Program.cs
+++ Kuafor_web/Program.cs
@@ -1,7 +1,21 @@
+
+using Kuafor_web.Models;
+ 
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BerberDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(ayar =>
+    {
+        ayar.LoginPath = "/Giris/GirisYap";
+    }
+    );
 
 var app = builder.Build();
 
@@ -9,19 +23,16 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
-
-app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=AnaSayfa}/{id=1}");
 
 app.Run();
